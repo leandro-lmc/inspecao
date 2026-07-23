4d617f2bfc4163e56013d005c069bbd58559fff6
leandro-lmc <leandro.marqueslmc@gmail.com>
2026-07-23 16:03:21 -0300

    Inclusão do login

--- src/modules/administrador/controllers/admin.controller.js
+++ src/modules/administrador/controllers/admin.controller.js
@@ -1,32 +1,105 @@
-
 import bcrypt from "bcryptjs";
-import AdminModel from "..models/AdminModel.js"; 
+import jwt from "jsonwebtoken";
+import AdminModel from "..models/AdminModel.js";
 
 class AdminController {
-    static async cadastrar(requisicao, resposta){
-        try {
-            const { id, nome, email, senha } = requisicao.body
-            if(!id || !nome || !email || !senha){
-                return resposta.status(400).json({mensagem:"Todos os campos são obrigatórios!"})
-            }
-            const totalAdmin = await AdminModel.contarAdmins()
-            if(totalAdmin > 0){
-                return resposta.status(409).json({mensagem:"Já existe um administrador cadastrado com este email!"})
-            }
-            if(senha.length < 8){
-                return resposta.status(403).json({mensagem:"A senha deve ter no mínimo 8 caracteres!"})
-            }
-            const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,32}$/
-            if(!regex.test(senha)){
-                return resposta.status(403).json({mensagem:"Senha invalida! Sua senha deve conter pelo menos: 1 letra maiúscula, 1 letra minúscula, 1 número, 1 caractere especial (ex: @, #, $, %)!"})
-            }
-            const salt = bcrypt.genSaltSync(10);
-            const hashSenha = bcrypt.hashSync(senha, salt);
-            const admin = await AdminModel.cadastrar(id, nome, email, senha=hashSenha)
-            return resposta.status(201).json({mensagem:"Usuário administrador cadastrado com sucesso!"})
-
-        } catch(error){
-            return resposta.status(500).json({mensagem:"Erro ao cadastrar administrador!", erro:error.message})             
-        }
+  static async cadastrar(requisicao, resposta) {
+    try {
+      const { id, nome, email, senha } = requisicao.body;
+      if (!id || !nome || !email || !senha) {
+        return resposta
+          .status(400)
+          .json({ mensagem: "Todos os campos são obrigatórios!" });
+      }
+      const totalAdmin = await AdminModel.contarAdmins();
+      if (totalAdmin > 0) {
+        return resposta
+          .status(409)
+          .json({
+            mensagem: "Já existe um administrador cadastrado com este email!",
+          });
+      }
+      if (senha.length < 8) {
+        return resposta
+          .status(403)
+          .json({ mensagem: "A senha deve ter no mínimo 8 caracteres!" });
+      }
+      const regex =
+        /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,32}$/;
+      if (!regex.test(senha)) {
+        return resposta
+          .status(403)
+          .json({
+            mensagem:
+              "Senha invalida! Sua senha deve conter pelo menos: 1 letra maiúscula, 1 letra minúscula, 1 número, 1 caractere especial (ex: @, #, $, %)!",
+          });
+      }
+      const regexEmail =
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
+      if (!regexEmail.test(email)) {
+        return resposta
+          .status(403)
+          .json({
+            mensagem: "Email invalido! Por favor, insira um email válido!",
+          });
+      }
+      const salt = bcrypt.genSaltSync(10);
+      const hashSenha = bcrypt.hashSync(senha, salt);
+      await AdminModel.cadastrar(id, nome, email, (senha = hashSenha));
+      return resposta
+        .status(201)
+        .json({ mensagem: "Usuário administrador cadastrado com sucesso!" });
+    } catch (error) {
+      return resposta
+        .status(500)
+        .json({
+          mensagem: "Erro ao cadastrar administrador!",
+          erro: error.message,
+        });
     }
+  }
+  static async login(requisicao, resposta) {
+    try {
+      const { email, senha } = requisicao.body;
+      if (!email || !senha) {
+        return resposta
+          .status(403)
+          .json({ mensagem: "Todos os campos são obrigatórios!" });
+      }
+      const administrador = await AdminModel.buscarPorEmail(email);
+      if (administrador.length === 0) {
+        return resposta
+          .status(404)
+          .json({ mensagem: "Administrador não encontrado!" });
+      }
+      if (administrador.ativo === false) {
+        return resposta
+          .status(403)
+          .json({ mensagem: "Administrador inativo!" });
+      }
+      const VerificarSenha = bcrypt.compareSync(senha, administrador.senha);
+      if (!VerificarSenha) {
+        return resposta
+          .status(403)
+          .json({ mensagem: "E-mail ou senha incorreta!" });
+      }
+      const token = jwt.sign(
+        {
+          id: administrador.id,
+          nome: administrador.nome,
+          email: administrador.email,
+        },
+        process.env.JWT_SECRET,
+        { 
+            expiresIn: process.env.JWT_TEMPO_EXPIRACAO || "1h" 
+        },
+      );
+      return resposta
+        .status(200)
+        .json({ mensagem: "Usuário autenticado com sucesso!", token });
+
+    } catch (error) {
+      return resposta.status(500).json({mensagem: "Erro interno ao realizar login!", erro: error.message});
+    }
+  }
 }
